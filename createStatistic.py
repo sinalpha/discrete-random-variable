576d6a9f0f11fe91f12e8f32b8fc5a0335cf4e65
Sinalpha <59336397+sinalpha@users.noreply.github.com>
2022-07-03 18:36:51 +0900

    Add files via upload

---
+++ createStatistic.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def createStatic(RV, pro, colLen):
+    fileName_ = "output.jpg"
+    x = np.arange(colLen)
+    plt.bar(x, pro)
+    plt.xticks(x, RV)
+    plt.savefig(fileName_)
---
+++ writeHTML.py
@@ -0,0 +1,57 @@
+import os
+
+textCSS = '''
+body, html {
+    width : 100%;
+    height : 100%;
+    margin : 0;
+}
+body{
+    display: flex;
+    flex-dirction:column;
+    justify-content: center;
+    align-items : center;
+}
+#wrap-value{
+}
+
+'''
+
+
+def openHTML(mean, variance, SD, static):
+    textHTML = f'''
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <link href="statistics.css" rel="stylesheet">
+    <title>statistics</title>
+</head>
+<body>
+    <img src="output.jpg">
+    <div id="wrap-value">
+        <div>mean is {mean}</div> 
+        <div>variance is {variance}</div> 
+        <div>standard Deviation is {SD}</div>
+    </div>
+</body>
+</html>
+'''
+
+    f = open('statistics.html', 'w')
+    f.write(textHTML)
+    f.close
+    f = open('statistics.css', 'w')
+    f.write(textCSS)
+    f.close
+    os.startfile('statistics.html')
+
+
+def delfiles():
+    answer = input(
+        "Do you want to keep created files saved? 1 : yes 2 : no (default : 1) : ")
+    if(answer == 2):
+        return
+    else:
+        os.system("del statistics.html statistics.css output.jpg ")
+        return
