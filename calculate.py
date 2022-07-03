ccf0830ab7cc3e1d9b8c41db4f922c206879c8c6
Sinalpha <59336397+sinalpha@users.noreply.github.com>
2022-07-03 21:23:58 +0900

    Update calculate.py

--- calculate.py
+++ calculate.py
@@ -1,17 +1,17 @@
 import math 
 
 def variance1(RV, pro):
-    sum, mean_ = 0,  mean(RV, pro)
+    sum, excepted_ = 0,  excepted(RV, pro)
     for RV_, pro_ in zip(RV, pro):
-        sum += ((RV_ - mean_) ** 2) * pro_
+        sum += ((RV_ - excepted_) ** 2) * pro_
     return sum
 
 def variance2(RV, pro):
     RVSquare = [ i**2 for i in RV]
-    return mean(RVSquare, pro) - mean(RV, pro) **2
+    return excepted(RVSquare, pro) - excepted(RV, pro) **2
     
 
-def mean(RV, pro):
+def excepted(RV, pro):
     sum = 0
     for RV_, pro_ in zip(RV, pro):
         sum += RV_ * pro_
